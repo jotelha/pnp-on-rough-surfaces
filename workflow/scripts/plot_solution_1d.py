db4a545be2eba2b721c8bda045d626a7c905222f
Johannes Laurin Hoermann <johannes.hoermann@imtek.uni-freiburg.de>
2024-08-22 09:13:49 +0200

    ENH: 1d solution miniplot alright

--- workflow/scripts/plot_solution_1d.py
+++ workflow/scripts/plot_solution_1d.py
@@ -34,7 +34,7 @@ I = ionic_strength(z=number_charges, c=reference_concentrations)
 
 debye_length = lambda_D(ionic_strength=I, temperature=temperature, relative_permittivity=relative_permittivity)
 
-length = height_normalized*debye_length
+length = 0.5*height_normalized*debye_length
 
 gas_constant = sc.value('molar gas constant')
 faraday_constant = sc.value('Faraday constant')
@@ -46,53 +46,55 @@ potential_bias = potential_bias_SI / thermal_voltage
 pnp = PoissonNernstPlanckSystem(
     c=reference_concentrations,
     z=number_charges,
-    L=length, delta_u=potential_bias)
+    L=length, delta_u=potential_bias_SI)
 
 pnp.use_standard_interface_bc()
 ui, nij, _ = pnp.solve()
 
 x = np.linspace(0, length, 100)
 
-def make_patch_spines_invisible(ax):
-    ax.set_frame_on(True)
-    ax.patch.set_visible(False)
-    for sp in ax.spines.values():
-        sp.set_visible(False)
+fig, ax1 = plt.subplots(nrows=1,ncols=1)
 
-# %%
-fig, (ax1,ax4) = plt.subplots(nrows=1,ncols=1)
+ax1.axvline(x=debye_length, label='Debye length', color='grey', linestyle=':', linewidth=0.5)
+# ax1.text(debye_length*1.02, 0.01, "Debye length", rotation=90)
 
-ax1.axvline(x=debye_length, label='Debye length', color='grey', linestyle=':')
-ax1.text(debye_length*1.02, 0.01, "Debye length", rotation=90)
-
-ax1.plot(pnp.grid, pnp.potential, marker='', color='tab:red', label='potential, PNP', linewidth=1, linestyle='-')
+p_potential, = ax1.plot(pnp.grid, pnp.potential, marker='', color='tab:red', label='potential, PNP', linewidth=1, linestyle='-')
 
 ax2 = ax1.twinx()
-ax2.plot(x, np.ones(x.shape)*reference_concentrations[0], label='bulk concentration', color='grey', linestyle=':')
+ax2.plot(x, np.ones(x.shape)*reference_concentrations[0], label='bulk concentration', color='grey', linestyle=':',  linewidth=0.5)
 ax2.text(10, reference_concentrations[0]*1.1, "bulk concentration")
 
-ax2.plot(pnp.grid, pnp.concentration[0], marker='', color='tab:orange', label='Na+, PNP', linewidth=2, linestyle='-')
+ax2.plot(pnp.grid, pnp.concentration[0], marker='', color='tab:orange', label='Na+, PNP', linewidth=1.5, linestyle=':')
+ax2.plot(pnp.grid, pnp.concentration[1], marker='', color='tab:blue', label='Cl-, PNP', linewidth=1.5, linestyle=':')
 
-ax2.plot(pnp.grid, pnp.concentration[1], marker='', color='tab:blue', label='Cl-, PNP', linewidth=2, linestyle='-')
+tkw = dict(size=4, width=1.5)
+ax1.tick_params(axis='y', colors=p_potential.get_color(), **tkw)
+ax1.tick_params(axis='x', **tkw)
 
-ax3 = ax1.twinx()
-# Offset the right spine of ax3.  The ticks and label have already been
-# placed on the right by twinx above.
-ax3.spines["right"].set_position(("axes", 1.1))
+ax1.yaxis.label.set_color(p_potential.get_color())
 
-# Having been created by twinx, ax3 has its frame off, so the line of its
-# detached spine is invisible. First, activate the frame but make the patch
-# and spines invisible.
-make_patch_spines_invisible(ax3)
+ax1.set_xlabel("Height")
+ax1.set_ylabel("Potential")
+ax2.set_ylabel("Concentration")
 
-# Second, show the right spine.
-ax3.spines["right"].set_visible(True)
+ax1.set_xticks(
+    ticks=[0, debye_length],
+    labels=[0, "$\lambda_D$"]
+)
 
-ax3.plot(pnp.grid, pnp.charge_density, label='charge density, PNP', color='grey', linewidth=1, linestyle='-')
+ax1.set_yticks(
+    ticks=[0, potential_bias_SI],
+    labels=[0, "$U_T$"]
+)
 
-ax1.legend(loc='upper right',  bbox_to_anchor=(-0.1,1.02), fontsize=15)
-ax2.legend(loc='center right', bbox_to_anchor=(-0.1,0.5),  fontsize=15)
-ax3.legend(loc='lower right',  bbox_to_anchor=(-0.1,-0.02), fontsize=15)
+ax2.set_yticks(
+    ticks=[0, reference_concentrations[0]],
+    labels=[0, "$c_\mathrm{bulk}$"]
+)
+
+fig.set_size_inches(2, 1.6)
+
+fig.subplots_adjust(bottom=0.3, left=0.25, right=0.7)
 
 fig.tight_layout()
 fig.savefig(svg_file)
