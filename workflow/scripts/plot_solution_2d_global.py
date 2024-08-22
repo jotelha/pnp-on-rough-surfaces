ENH: global 2d plots with correct scaling

workflow/scripts/plot_solution_2d_global.py
@@ -14,12 +14,14 @@ checkpoint_bp = input.interpolated_solution_checkpoint_bp
 
 scale_factor = 2
 
-xscale = 0.05
-x_offset_factor = 1.4
+xscale = 0.02
+x_offset_factor = 0.93
 y_offset = 2
 z_offset = 15
 contour_label_x_offset = - 3.6
 contour_label_y_offset = 0.12
+
+x_clip_width = 400
 
 import logging
 
@@ -103,8 +105,8 @@ focal_point = [
 logger.info("focal point: %s", focal_point)
 
 # clip in x direction
-upper_x_clip = focal_point[0] + 80
-lower_x_clip = focal_point[0] - 80
+upper_x_clip = focal_point[0] + x_clip_width/2
+lower_x_clip = focal_point[0] - x_clip_width/2
 
 upper_y_clip = focal_point[1] - y_offset + 5
 lower_y_clip = focal_point[1] - y_offset - 1
@@ -267,6 +269,25 @@ xruler = plotter.add_ruler(pointa, pointb,
                            label_format='%.0f',
                            font_size_factor=0.8,
                            label_size_factor=0.7)
+# keyword scale does not exist in this version
+
+xruler_range = xruler.GetRange()
+logger.info("xruler range: %s", xruler_range)
+x_range = np.array([bounds[0], bounds[1]])
+logger.info("x range: %s", x_range)
+xruler_scale_factor = (xruler_range[1] - xruler_range[0])/(x_range[1]-x_range[0])
+logger.info("xruler range / x range scale factor: %g", xruler_scale_factor)
+xruler.SetRange(bounds[0], bounds[1])
+new_xruler_range = xruler.GetRange()
+
+number_of_ticks = int((x_range[1]-x_range[0])/50)+1
+logger.info("number of ticks: %s", number_of_ticks)
+logger.info("new  xruler range: %s", new_xruler_range)
+
+xruler.AdjustLabelsOff()
+# xruler.SetNumberOfMinorTicks(number_of_minor_ticks)
+xruler.SetNumberOfLabels(number_of_ticks)
+
 title_text_property = xruler.GetTitleTextProperty()
 title_text_property.BoldOff()
 title_text_property.ItalicOff()
@@ -274,8 +295,6 @@ title_text_property.ItalicOff()
 label_text_property = xruler.GetLabelTextProperty()
 label_text_property.BoldOff()
 label_text_property.ItalicOff()
-
-xruler.SetRange(bounds[0], bounds[1])
 
 pointa = [bounds[0] * xscale - 0.3, 0, 0]
 pointb = [bounds[0] * xscale - 0.3, 5, 0]
@@ -300,12 +319,14 @@ plotter.screenshot(output.potential_png)
 
 # concentrations
 
-xscale = 0.05
-x_offset_factor = 1.2
+xscale = 0.02
+x_offset_factor = 0.93
 y_offset = 2
 z_offset = 15
 contour_label_x_offset = - 3.6
-contour_label_y_offset = 0.08
+contour_label_y_offset = 0.05
+
+x_clip_width = 400
 
 for i, concentration_function in enumerate(concentration_functions):
     grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
@@ -325,8 +346,8 @@ for i, concentration_function in enumerate(concentration_functions):
     logger.info("focal point: %s", focal_point)
 
     # clip in x direction
-    upper_x_clip = focal_point[0] + 80
-    lower_x_clip = focal_point[0] - 80
+    upper_x_clip = focal_point[0] + x_clip_width / 2
+    lower_x_clip = focal_point[0] - x_clip_width / 2
 
     upper_y_clip = focal_point[1] - y_offset + 5
     lower_y_clip = focal_point[1] - y_offset - 1
@@ -368,7 +389,9 @@ for i, concentration_function in enumerate(concentration_functions):
     ]
     logger.info("camera position: %s", focal_point)
 
-    for level, value in zip(levels, contour_values[::-1]):
+    if contour_values[0] > 1: # weird adjustment for ordering
+         contour_values = contour_values[::-1]
+    for level, value in zip(levels, contour_values):
         pt = np.mean(level.points, axis=0)
         pt[0] = (camera_position[0] + contour_label_x_offset) / xscale
         pt[1] += contour_label_y_offset
@@ -376,7 +399,7 @@ for i, concentration_function in enumerate(concentration_functions):
         logger.info("contour %.2f: coordinates %s", value, pt)
         pts.append(pt)
 
-    contour_labels = [f'{v:.2f}' for v in contour_values[::-1]]
+    contour_labels = [f'{v:.2f}' for v in contour_values]
     contour_label_coordinates = np.array(pts)
 
     logger.info("Contour labels: %s", contour_labels)
@@ -452,8 +475,8 @@ for i, concentration_function in enumerate(concentration_functions):
         logger.info(f"Window size: {window_size}")
 
         viewport_coords = [
-            (clamped_ndc_coords[0] + 1) * 0.5 * window_size[0],  # X viewport
-            (clamped_ndc_coords[1] + 1) * 0.5 * window_size[1]  # Y viewport
+            (clamped_ndc_coords[0] + 1) * 0.5 * window_size[0] * scale_factor,  # X viewport
+            (clamped_ndc_coords[1] + 1) * 0.5 * window_size[1] * scale_factor # Y viewport
         ]
 
         logger.info(f"Viewport coordinates: {viewport_coords}")
@@ -492,6 +515,15 @@ for i, concentration_function in enumerate(concentration_functions):
                                font_size_factor=0.8,
                                label_size_factor=0.7)
 
+    xruler_range = xruler.GetRange()
+    logger.info("xruler range: %s", xruler_range)
+    x_range = np.array([bounds[0], bounds[1]])
+    logger.info("x range: %s", x_range)
+    xruler_scale_factor = (xruler_range[1] - xruler_range[0])/(x_range[1]-x_range[0])
+    logger.info("xruler range / x range scale factor: %g", xruler_scale_factor)
+
+    xruler.SetRange(x_range[0], x_range[1])
+
     title_text_property = xruler.GetTitleTextProperty()
     title_text_property.BoldOff()
     title_text_property.ItalicOff()
@@ -500,7 +532,21 @@ for i, concentration_function in enumerate(concentration_functions):
     label_text_property.BoldOff()
     label_text_property.ItalicOff()
 
+    xruler_range = xruler.GetRange()
+    logger.info("xruler range: %s", xruler_range)
+    x_range = np.array([bounds[0], bounds[1]])
+    logger.info("x range: %s", x_range)
+    xruler_scale_factor = (xruler_range[1] - xruler_range[0]) / (x_range[1] - x_range[0])
+    logger.info("xruler range / x range scale factor: %g", xruler_scale_factor)
     xruler.SetRange(bounds[0], bounds[1])
+    new_xruler_range = xruler.GetRange()
+
+    number_of_ticks = int(np.round(x_range[1] - x_range[0]) / 50) + 1
+    logger.info("number of ticks: %s", number_of_ticks)
+    logger.info("new  xruler range: %s", new_xruler_range)
+
+    xruler.AdjustLabelsOff()
+    xruler.SetNumberOfLabels(number_of_ticks)
 
     pointa = [bounds[0] * xscale - 0.3, 0, 0]
     pointb = [bounds[0] * xscale - 0.3, 5, 0]